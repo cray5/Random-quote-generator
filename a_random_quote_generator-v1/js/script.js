e4a3f2751579973dd73e3c369ce5b23964c8da67
Chintan Ray <cray51992@gmail.com>
2019-05-29 21:35:51 +1000

    Adding quote data
    - Added an Array 'quotes' that has 5
       object elements.
    
    - Created getRandomQuote function
       that gets random quotes from the
       array 'quotes'.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -15,17 +15,52 @@ project 1 - A Random Quote Generator
   Use console.log() to log your array of quotes to the console.
 ***/
 
+var quotes = [
+  { 
+    quote: "I don't believe that if you do good, good things will happen. Everything is completely accidental and random. Sometimes bad things happen to very good people and sometimes good things happen to bad people. But at least if you try to do good things, then you're spending your time doing something worthwhile.",
+    source: "Helen Mirren",
+    citation: "www.brainyquote.com/topics/random",
+    year: 2010,
+  },
+  { 
+  quote: "Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.",
+  source: "Sushant Singh Rajput",
+  citation: "www.brainyquote.com/topics/random",
+  year:2012 ,
+  },
+  { 
+    quote: "Goals transform a random walk into a chase.",
+    source: "Mihaly Csikszentmihalyi",
+    citation: "www.brainyquote.com/topics/random",
+    year: 1992,
+  },
+  { 
+    quote: "Life just doesn't care about our aspirations, or sadness. It's often random, and it's often stupid and it's often completely unexpected, and the closures and the epiphanies and revelations we end up receiving from life, begrudgingly, rarely turn out to be the ones we thought.",
+    source: "Khaled Hosseini",
+    citation: "www.brainyquote.com/topics/random",
+    year: 1990 ,
+  },
+  { 
+    quote: "There are good people who are dealt a bad hand by fate, and bad people who live long, comfortable, privileged lives. A small twist of fate can save or end a life; random chance is a permanent, powerful player in each of our lives, and in human history as well.",
+    source: "Jeff Greenfield",
+    citation: "www.brainyquote.com/topics/random",
+    year: 2002,
+  },
+];
 
-
-
+console.log(quotes);
 /***
   Create the `getRandomQuote` function to:
    - Create a variable to store a random number 
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
 
-
-
+function getRandomQuote(prod) {
+  var randomNum = Math.floor(Math.random() * 5);
+  return prod[randomNum].quote;
+  
+};
+console.log(getRandomQuote(quotes));
 
 /***
   Create the `printQuote` function to: 
@@ -50,7 +85,7 @@ project 1 - A Random Quote Generator
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+/*document.getElementById('loadQuote').addEventListener("click", printQuote, false);*/
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
